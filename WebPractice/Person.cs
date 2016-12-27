ddffa2d0d6b40bf38f01f83602cf710e89a43144
levj1 <levj1@yahoo.com>
2016-12-27 15:29:16 -0500

    Razor c#
    Practice razor

---
+++ WebPractice/Person.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebPractice
+{
+    public class Person
+    {
+        public int ID { get; set; }
+        public string FullName { get; set; }
+    }
+}
